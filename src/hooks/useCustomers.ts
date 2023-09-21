232ad6658d29a51b7d2fe2f214d736bc1bcc2ef8
Eduardo Ari <eduardoariturconi@gmail.com>
2023-09-21 17:20:54 -0300

    fix lint

--- src/hooks/useCustomers.ts
+++ src/hooks/useCustomers.ts
@@ -11,12 +11,104 @@ export type CustomerData = {
   sex: PossibleSex
 }
 
+const exerciseLists = {
+  'woman-lose-weight-a-id': {
+    id: 'woman-lose-weight-a-id',
+    name: 'Treino de superiores feminino',
+    exercises: [
+      {
+        name: 'Pulldown',
+        numberOfReps: 10,
+        numberOfSets: 4,
+      },
+      {
+        name: 'Rosca',
+        numberOfReps: 10,
+        numberOfSets: 4,
+      },
+      // Add more exercises for women's fat loss back/biceps here
+    ],
+  },
+  'woman-lose-weight-c-id': {
+    id: 'woman-lose-weight-c-id',
+    name: 'Treino de inferiores feminino',
+    exercises: [
+      {
+        name: 'Agachamento',
+        numberOfReps: 10,
+        numberOfSets: 4,
+      },
+      {
+        name: 'Elevação pélvica',
+        numberOfReps: 10,
+        numberOfSets: 4,
+      },
+      // Add more exercises for women's fat loss legs here
+    ],
+  },
+  'man-lose-weight-a-id': {
+    id: 'man-lose-weight-a-id',
+    name: 'Treino masculino costas/bíceps',
+    exercises: [
+      {
+        name: 'Remada',
+        numberOfReps: 10,
+        numberOfSets: 4,
+      },
+      {
+        name: 'Rosca',
+        numberOfReps: 10,
+        numberOfSets: 4,
+      },
+      // Add more exercises for men's fat loss back/biceps here
+    ],
+  },
+  'man-lose-weight-b-id': {
+    id: 'man-lose-weight-b-id',
+    name: 'Treino maculino peito/tríceps',
+    exercises: [
+      {
+        name: 'Supino',
+        numberOfReps: 10,
+        numberOfSets: 4,
+      },
+      {
+        name: 'Tríceps francês',
+        numberOfReps: 10,
+        numberOfSets: 4,
+      },
+      // Add more exercises for men's fat loss chest/triceps here
+    ],
+  },
+  'man-lose-weight-c-id': {
+    id: 'man-lose-weight-c-id',
+    name: 'Treino masculino pernas',
+    exercises: [
+      {
+        name: 'Agachamento',
+        numberOfReps: 10,
+        numberOfSets: 4,
+      },
+      {
+        name: 'Leg Press',
+        numberOfReps: 10,
+        numberOfSets: 4,
+      },
+      // Add more exercises for men's fat loss legs here
+    ],
+  },
+  // Add more subcategories as needed
+}
+type ExerciseSet = (typeof exerciseLists)['man-lose-weight-a-id']
+
 type CustomerWithExercises = CustomerData & {
   exercises: ExerciseSet[]
 }
 
 export const useCustomers = () => {
-  const [customers, setCustomers] = useState<Record<string, CustomerWithExercises>>({})
+  const [customers, setCustomers] = useState<
+    Record<string, CustomerWithExercises>
+  >({})
 
   const getOne = (id: string): CustomerWithExercises | undefined => {
     return customers[id]
@@ -28,14 +120,21 @@ export const useCustomers = () => {
 
   const addCustomer = (newCustomer: CustomerData) => {
     const exercisesMapper: Record<PossibleSex, Array<ExerciseSet>> = {
-      F: [exerciseLists['woman-lose-weight-a-id'], exerciseLists['woman-lose-weight-c-id']],
-      M: [exerciseLists['man-lose-weight-a-id'], exerciseLists['man-lose-weight-b-id'], exerciseLists['man-lose-weight-c-id']],
+      F: [
+        exerciseLists['woman-lose-weight-a-id'],
+        exerciseLists['woman-lose-weight-c-id'],
+      ],
+      M: [
+        exerciseLists['man-lose-weight-a-id'],
+        exerciseLists['man-lose-weight-b-id'],
+        exerciseLists['man-lose-weight-c-id'],
+      ],
     }
     const updatedCustomers = Object.assign(customers, {
-      [newCustomer.id]: { 
+      [newCustomer.id]: {
         ...newCustomer,
-        exercises: exercisesMapper[newCustomer.sex]
-      }
+        exercises: exercisesMapper[newCustomer.sex],
+      },
     })
     setCustomers(updatedCustomers)
 
@@ -44,93 +143,3 @@ export const useCustomers = () => {
 
   return { customers, addCustomer, getOne }
 }
-
-type ExerciseSet = typeof exerciseLists['man-lose-weight-a-id']
-const exerciseLists = {
-  "woman-lose-weight-a-id": {
-      id: "woman-lose-weight-a-id",
-      name: "Treino de superiores feminino",
-      exercises: [
-        {
-          name: "Pulldown",
-          numberOfReps: 10,
-          numberOfSets: 4,
-        },
-        {
-          name: "Rosca",
-          numberOfReps: 10,
-          numberOfSets: 4,
-        },
-        // Add more exercises for women's fat loss back/biceps here
-      ],
-    },
-    "woman-lose-weight-c-id": {
-      id: "woman-lose-weight-c-id",
-      name: "Treino de inferiores feminino",
-      exercises: [
-        {
-          name: "Agachamento",
-          numberOfReps: 10,
-          numberOfSets: 4,
-        },
-        {
-          name: "Elevação pélvica",
-          numberOfReps: 10,
-          numberOfSets: 4,
-        },
-        // Add more exercises for women's fat loss legs here
-      ],
-    },  
-  "man-lose-weight-a-id": {
-    id: "man-lose-weight-a-id",
-    name: "Treino masculino costas/bíceps",
-    exercises: [
-      {
-        name: "Remada",
-        numberOfReps: 10,
-        numberOfSets: 4,
-      },
-      {
-        name: "Rosca",
-        numberOfReps: 10,
-        numberOfSets: 4,
-      },
-      // Add more exercises for men's fat loss back/biceps here
-    ],
-  },
-  "man-lose-weight-b-id": {
-    id: "man-lose-weight-b-id",
-    name: "Treino maculino peito/tríceps",
-    exercises: [
-      {
-        name: "Supino",
-        numberOfReps: 10,
-        numberOfSets: 4,
-      },
-      {
-        name: "Tríceps francês",
-        numberOfReps: 10,
-        numberOfSets: 4,
-      },
-      // Add more exercises for men's fat loss chest/triceps here
-    ],
-  },
-  "man-lose-weight-c-id": {
-    id: "man-lose-weight-c-id",
-    name: "Treino masculino pernas",
-    exercises: [
-      {
-        name: "Agachamento",
-        numberOfReps: 10,
-        numberOfSets: 4,
-      },
-      {
-        name: "Leg Press",
-        numberOfReps: 10,
-        numberOfSets: 4,
-      },
-      // Add more exercises for men's fat loss legs here
-    ],
-  },
-  // Add more subcategories as needed
-};
